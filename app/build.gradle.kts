aa00bbb05ab11c634aa70bcf149886bdef559312
Ezio (Daniel Opazo) <133083000+DaniAuditore@users.noreply.github.com>
2026-04-19 23:53:56 -0400

    chore: add unit test dependencies (JUnit, MockK, Coroutines Test)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,9 @@ dependencies {
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
